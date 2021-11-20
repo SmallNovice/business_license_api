9fb074ca8912b59910c0be001ea263736139155a
SmallNovice <691513006@qq.com>
2021-11-21 02:21:25 +0800

    fix: change get response to post response

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :identification do
     resources :business_license_identification do
-      get 'identify', on: :collection
+      post :identify, on: :collection
     end
   end
 end
